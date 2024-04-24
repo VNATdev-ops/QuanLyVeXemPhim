684d4310bae0891385b2f12bb99b70aa259bd6d0
VuNguyenAnhTuan <vuanhtuan18092004@gmail.com>
2024-04-25 01:08:51 +0700

    update Ctrl + C

---
+++ QuanLyVeXemPhim/Models/CLichSuTichDiem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyVeXemPhim.Models
+{
+    internal class CLichSuTichDiem
+    {
+        private int lichSuID;
+        private int soDiemTichLuy;
+        private DateTime thoiGianTichLuy;
+        private int tongDiemTichLuy;
+        private int nguoiDungID;
+
+        public CLichSuTichDiem()
+        {
+        }
+
+        public CLichSuTichDiem(int lichSuID, int soDiemTichLuy, DateTime thoiGianTichLuy, int tongDiemTichLuy, int nguoiDungID)
+        {
+            this.lichSuID = lichSuID;
+            this.soDiemTichLuy = soDiemTichLuy;
+            this.thoiGianTichLuy = thoiGianTichLuy;
+            this.tongDiemTichLuy = tongDiemTichLuy;
+            this.nguoiDungID = nguoiDungID;
+        }
+
+        public CLichSuTichDiem(int lichSuID, int nguoiDungID)
+        {
+            this.lichSuID = lichSuID;
+            this.nguoiDungID = nguoiDungID;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CLichSuTichDiem diem &&
+                   lichSuID == diem.lichSuID;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(lichSuID);
+        }
+    }
+}
---
+++ QuanLyVeXemPhim/Models/CNguoiDung.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyVeXemPhim.Models
+{
+    internal class CNguoiDung
+    {
+        private int nguoiDungID;
+        private string ten;
+        private string matKhau;
+        private DateTime ngaySinh;
+        private string gioiTinh;
+        private string khuVuc;
+        private string email;
+        private string loaiTaiKhoan;
+
+        public CNguoiDung()
+        {
+        }
+
+        public CNguoiDung(int nguoiDungID, string ten, string matKhau, DateTime ngaySinh, string gioiTinh, string khuVuc, string email, string loaiTaiKhoan)
+        {
+            this.nguoiDungID = nguoiDungID;
+            this.ten = ten;
+            this.matKhau = matKhau;
+            this.ngaySinh = ngaySinh;
+            this.gioiTinh = gioiTinh;
+            this.khuVuc = khuVuc;
+            this.email = email;
+            this.loaiTaiKhoan = loaiTaiKhoan;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CNguoiDung dung &&
+                   nguoiDungID == dung.nguoiDungID;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nguoiDungID);
+        }
+    }
+}
