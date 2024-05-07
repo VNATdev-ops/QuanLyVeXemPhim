fe86e188fc567c0098405f1d46fc74ec5f20c29e
$$$ <152199305+Money-NNT@users.noreply.github.com>
2024-05-07 17:35:32 +0700

    update: chỗ ngồi

--- QuanLyVeXemPhim/Controller/CtrlChoNgoi.cs
+++ QuanLyVeXemPhim/Controller/CtrlChoNgoi.cs
@@ -1,4 +1,5 @@
-﻿using QuanLyVeXemPhim.Utils;
+﻿using QuanLyVeXemPhim.Models;
+using QuanLyVeXemPhim.Utils;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -16,6 +17,99 @@ namespace QuanLyVeXemPhim.Controller
             ConnectDB cnnDB = new ConnectDB();
             cnn = cnnDB.getConnection();
         }
+        public List<CChoNgoi> findAll()
+        {
+            string sql = "select * from chongoi";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Connection = cnn;
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<CChoNgoi> arrs = new List<CChoNgoi>();
+            while (reader.Read())
+            {
+                CChoNgoi s = new CChoNgoi();
+                s.IDChoNgoi = reader.GetString(0);
+                s.Phong = new CPhongChieu();
+                s.Phong.IDPhong = reader.GetString(1);
+                s.LoaiChoNgoi = reader.GetString(2);
+                s.ViTri = reader.GetString(3);
+                s.TrangThai = reader.GetString(4);
+                arrs.Add(s);
+            }
+            reader.Close();
+            return arrs;
+        }
+        public bool insert(CChoNgoi obj)
+        {
+            try
+            {
+                string sql = "insert into chongoi values (@IDChoNgoi, @IDPhong, @LoaiChoNgoi,@ViTri,@TrangThai)";
+                SqlCommand cmd = new SqlCommand(sql);
+                cmd.Parameters.AddWithValue("@IDChoNgoi", obj.IDChoNgoi);
+                cmd.Parameters.AddWithValue("@IDPhong", obj.Phong.IDPhong);
+                cmd.Parameters.AddWithValue("@LoaiChoNgoi", obj.LoaiChoNgoi);
+                cmd.Parameters.AddWithValue("@ViTri", obj.ViTri);
+                cmd.Parameters.AddWithValue("@TrangThai", obj.TrangThai);
 
+                cmd.Connection = cnn;
+                int n = cmd.ExecuteNonQuery();
+                return (n > 0);
+            }
+            catch { return false; }
+
+        }
+        public bool delete(CChoNgoi obj)
+        {
+            try
+            {
+                string sql = "delete from chongoi where IDChoNgoi=@IDChoNgoi";
+                SqlCommand cmd = new SqlCommand(sql);
+                cmd.Parameters.AddWithValue("@IDChoNgoi", obj.IDChoNgoi);
+                cmd.Connection = cnn;
+                int n = cmd.ExecuteNonQuery();
+                return (n > 0);
+            }
+            catch { return false; }
+        }
+        public bool update(CChoNgoi obj)
+        {
+            try
+            {
+                string sql = "update chongoi set IDPhong=@IDPhong, LoaiChoNgoi=@LoaiChoNgoi, ViTri=@ViTri, TrangThai=@TrangThai where IDChoNgoi=@IDChoNgoi";
+                SqlCommand cmd = new SqlCommand(sql);
+                cmd.Parameters.AddWithValue("@IDChoNgoi", obj.IDChoNgoi);
+                cmd.Parameters.AddWithValue("@IDPhong", obj.Phong.IDPhong);
+                cmd.Parameters.AddWithValue("@LoaiChoNgoi", obj.LoaiChoNgoi);
+                cmd.Parameters.AddWithValue("@ViTri", obj.ViTri);
+                cmd.Parameters.AddWithValue("@TrangThai", obj.TrangThai);
+                cmd.Connection = cnn;
+                int n = cmd.ExecuteNonQuery();
+                return (n > 0);
+            }
+            catch { return false; }
+
+        }
+        public List<CChoNgoi> findCriteria(string DK)
+        {
+            string sql = " select * from chongoi where IDChoNgoi like @dk or IDPhong like @dk or LoaiChoNgoi like @dk or TrangThai like @dk or ViTri like @dk ";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@dk", "%" + DK + "%");
+            cmd.Connection = cnn;
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<CChoNgoi> arrs = new List<CChoNgoi>();
+            while (reader.Read())
+            {
+                CChoNgoi s = new CChoNgoi();
+                s.IDChoNgoi = reader.GetString(0);
+                s.Phong = new CPhongChieu();
+                s.Phong.IDPhong = reader.GetString(1);
+                s.LoaiChoNgoi = reader.GetString(2);
+                s.ViTri = reader.GetString(3);
+                s.TrangThai = reader.GetString(4);
+                arrs.Add(s);
+
+            }
+            reader.Close();
+            return arrs;
+        }
     }
 }
--- QuanLyVeXemPhim/Views/FChoNgoi.cs
+++ QuanLyVeXemPhim/Views/FChoNgoi.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuanLyVeXemPhim.Controller;
+using QuanLyVeXemPhim.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,187 @@ namespace QuanLyVeXemPhim.Views
 {
     public partial class FChoNgoi : Form
     {
+
+        CtrlChoNgoi ctrChoNgoi = new CtrlChoNgoi();
+        List<CChoNgoi> dsChoNgoi = new List<CChoNgoi>();
         public FChoNgoi()
         {
             InitializeComponent();
+            int width = lsvDanhSachCN.Width;
+            lsvDanhSachCN.Columns.Add("ID chỗ ngồi", 20 * width / 100);
+            lsvDanhSachCN.Columns.Add("ID phòng", 20 * width / 100);
+            lsvDanhSachCN.Columns.Add("Loại chỗ ngồi", 20 * width / 100);
+            lsvDanhSachCN.Columns.Add("Vị Trí", 20 * width / 100);
+            lsvDanhSachCN.Columns.Add("Trạng thái", 20 * width / 100);
+
+
+            lsvDanhSachCN.View = View.Details;
+            lsvDanhSachCN.FullRowSelect = true;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string maCN = txtIDchongoi.Text;
+            string maPhong = txtIDphong.Text;
+            string loaiChoNgoi = txtLoaiChoNgoi.Text;
+            string viTri = txtVitri.Text;
+            string trangThai = txtTrangThai.Text;
+            CChoNgoi choNgoi = new CChoNgoi();
+            choNgoi.IDChoNgoi = maCN;
+            choNgoi.Phong = new CPhongChieu();
+            choNgoi.Phong.IDPhong = maPhong;
+            choNgoi.LoaiChoNgoi = loaiChoNgoi;
+            choNgoi.ViTri = viTri;
+            choNgoi.TrangThai = trangThai;
+            if (ctrChoNgoi.insert(choNgoi))
+            {
+                string[] objCN = { maCN, maPhong, loaiChoNgoi, viTri, trangThai };
+                ListViewItem item = new ListViewItem(objCN);
+                lsvDanhSachCN.Items.Add(item);
+                dsChoNgoi.Add(choNgoi);
+                txtTongSo.Text = lsvDanhSachCN.Items.Count.ToString();
+                MessageBox.Show("Thêm thành công");
+            }
+            else
+                MessageBox.Show("Thêm thất bại!");
+        }
+
+        private void FChoNgoi_Load(object sender, EventArgs e)
+        {
+            dsChoNgoi = ctrChoNgoi.findAll();
+            foreach (CChoNgoi s in dsChoNgoi)
+            {
+                string[] obj = { s.IDChoNgoi, s.Phong.IDPhong, s.LoaiChoNgoi, s.ViTri, s.TrangThai };
+                ListViewItem item = new ListViewItem(obj);
+                lsvDanhSachCN.Items.Add(item);
+            }
+
+            txtTongSo.Text = lsvDanhSachCN.Items.Count.ToString();
+        }
+
+        private void lsvDanhSachCN_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ListViewItem item = lsvDanhSachCN.SelectedItems[0];
+                CChoNgoi choNgoi = new CChoNgoi();
+                choNgoi.IDChoNgoi = item.SubItems[0].Text;
+                int index = dsChoNgoi.IndexOf(choNgoi);
+                if (index < 0)
+                {
+                    return;
+                }
+                choNgoi = dsChoNgoi[index];
+
+                txtIDchongoi.Text = choNgoi.IDChoNgoi;
+                txtIDphong.Text = choNgoi.Phong.IDPhong;
+                txtLoaiChoNgoi.Text = choNgoi.LoaiChoNgoi;
+                txtVitri.Text = choNgoi.ViTri;
+                txtTrangThai.Text = choNgoi.TrangThai;
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                ListViewItem item = lsvDanhSachCN.SelectedItems[0];
+                CChoNgoi choNgoi = new CChoNgoi();
+                choNgoi.IDChoNgoi = item.SubItems[0].Text;
+                int index = dsChoNgoi.IndexOf(choNgoi);
+                if (index < 0)
+                {
+                    return;
+                }
+                choNgoi = dsChoNgoi[index];
+                if (ctrChoNgoi.delete(choNgoi))
+                {
+                    dsChoNgoi.Remove(choNgoi);
+                    lsvDanhSachCN.Items.Remove(item);
+                    MessageBox.Show("Xóa Thành Công");
+                }
+                else
+                {
+
+                    MessageBox.Show("Xóa Thất Bại.");
+                }
+
+                txtTongSo.Text = lsvDanhSachCN.Items.Count.ToString();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = lsvDanhSachCN.SelectedItems[0];
+            CChoNgoi choNgoi = new CChoNgoi();
+            choNgoi.IDChoNgoi = item.SubItems[0].Text;
+            int index = dsChoNgoi.IndexOf(choNgoi);
+            if (index < 0)
+            {
+                return;
+            }
+            choNgoi = dsChoNgoi[index];
+            choNgoi.Phong = new CPhongChieu();
+            choNgoi.Phong.IDPhong = txtIDphong.Text;
+            choNgoi.LoaiChoNgoi = txtLoaiChoNgoi.Text;
+            choNgoi.ViTri = txtVitri.Text;
+            choNgoi.TrangThai = txtTrangThai.Text;
+            if (ctrChoNgoi.update(choNgoi))
+            {
+                item.SubItems[1].Text = choNgoi.Phong.IDPhong;
+                item.SubItems[2].Text = choNgoi.LoaiChoNgoi;
+                item.SubItems[3].Text = choNgoi.ViTri;
+                item.SubItems[4].Text = choNgoi.TrangThai;
+                MessageBox.Show("Cập nhật thành công");
+            }
+            else
+            {
+                MessageBox.Show(" cập nhật thất bại");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            lsvDanhSachCN.Clear();
+            txtTongSo.Text = lsvDanhSachCN.Items.Count.ToString();
+
+        }
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string dkFind = txtTimKiem.Text;
+                dsChoNgoi = ctrChoNgoi.findCriteria(dkFind);
+
+                //xóa listview
+                lsvDanhSachCN.Items.Clear();
+                foreach (CChoNgoi s in dsChoNgoi)
+                {
+                    string[] obj = { s.IDChoNgoi, s.Phong.IDPhong, s.LoaiChoNgoi, s.ViTri, s.TrangThai };
+                    ListViewItem item = new ListViewItem(obj);
+                    lsvDanhSachCN.Items.Add(item);
+
+
+
+                }
+            }
+            catch { }
         }
     }
 }
